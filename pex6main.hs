1259e18eb83d88b72f98293d1fa03c653fd1c74d
Dkwon <C26Daewon.Kwon@afacademy.af.edu>
2025-11-21 20:18:39 -0700

    COmmit

--- pex6main.hs
+++ pex6main.hs
@@ -1,7 +1,7 @@
 -- pex6.hs 
 -- unKnot Haskell
 
--- name: 
+-- name: Daewon Kwon
 
 {- DOCUMENTATION:
 -}
@@ -10,8 +10,24 @@ unKnot tripCode
    | null tripCode = "unknot"
    | otherwise = "tangle - resulting trip code: " ++ (show tripCode)
 
--- typeIknot :: [(Char, Char)] -> String
--- typeIknot tripCode
+listLength :: [(Char, Char)] -> Int
+listLength a = case a of
+    [] -> 0
+    (a:as) -> 1 + listLength as
+
+typeIknot :: [(Char, Char)] -> [(Char, Char)]
+typeIknot tripCode
+   | null tripCode = []
+   | listLength tripCode < 2 = tripCode
+   | fst(head tripCode) == fst(head(tail tripCode)) = typeIknot (tail(tail tripCode))
+   | otherwise = head tripCode:typeIknot (tail tripCode)
+
+typeIIknot :: [(Char, Char)] -> [(Char, Char)]
+typeIIknot tripCode
+   | null tripCode = []
+   | listLength tripCode < 2 = tripCode
+   | snd(head tripCode) == snd(head(tail tripCode)) = typeIknot (tail(tail tripCode))
+   | otherwise = head tripCode:typeIknot (tail tripCode)
 
 main :: IO ()
 main = do
